ff662fdcf9d714896bb761927670311d5d5335fc
Seyma <sseymacakmakci@gmail.com>
2023-12-13 16:15:48 +0300

    Added recipe and user authentication components
    Added recipe management, and user authentication functionalities, including controllers for recipes and user authentication, corresponding view models, and HTML views for login, registration, and email verification.

---
+++ Presentation/Epicurious.MVC/ViewModels/AuthLoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Epicurious.MVC.ViewModels
+{
+    public class AuthLoginViewModel
+    {
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+
+    }
+}
---
+++ Presentation/Epicurious.MVC/ViewModels/AuthRegisterViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Epicurious.MVC.ViewModels
+{
+    public class AuthRegisterViewModel
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(2, ErrorMessage = "UserName must be at least 2 characters.")]
+        public string UserName { get; set; }
+
+        [Required]
+        [MinLength(6, ErrorMessage = "Password must be at least 6 characters.")]
+        public string Password { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string SurName { get; set; }
+
+        public DateTimeOffset? BirthDate { get; set; }
+    }
+}
+
+
