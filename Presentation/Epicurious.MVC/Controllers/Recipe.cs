a82c4ae1ac1b43ded4b84cdd5dd3c92d12666a27
kadirhan <61658086+Kadirhan54@users.noreply.github.com>
2023-12-13 14:11:31 +0300

    auth controller completed & migrations were applied

---
+++ Presentation/Epicurious.MVC/Controllers/Recipe.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Epicurious.MVC.Controllers
+{
+    public class Recipe : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
