3fede5cd28c9717dad987ba34ffddd4882dd2557
Tuhba <ozkayikcitugba@gmail.com>
2023-12-18 15:44:36 +0300

    Add Comment

---
+++ Presentation/Epicurious.MVC/Validators/CommentViewModelValidator.cs
@@ -0,0 +1,14 @@
+﻿using Epicurious.MVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Epicurious.MVC.Validators
+{
+    public class CommentViewModelValidator : AbstractValidator<CommentViewModel>
+    {
+        public CommentViewModelValidator()
+        {
+            RuleFor(x => x.Title).NotEmpty().WithMessage("Title is required").MinimumLength(10).WithMessage("Title is have to be longer than 10");
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Description is required").MinimumLength(10).WithMessage("Description is have to be longer than 10");
+        }
+    }
+}
---
+++ Presentation/Epicurious.MVC/ViewModels/CommentViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Epicurious.MVC.ViewModels
+{
+    public class CommentViewModel 
+    {
+        public string CreatedByUserId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
