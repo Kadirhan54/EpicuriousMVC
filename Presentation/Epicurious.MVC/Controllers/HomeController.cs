63bf13f29cb9845f6f415b07c1d6eafc5a1a329c
kadirhan <61658086+Kadirhan54@users.noreply.github.com>
2023-12-21 13:42:49 +0300

    added profile controller/page & ui improvements

--- Presentation/Epicurious.MVC/Controllers/HomeController.cs
+++ Presentation/Epicurious.MVC/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Epicurious.MVC.Models;
+using Epicurious.Persistence.UnitofWork;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,15 +8,17 @@ namespace Epicurious.MVC.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly UnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,UnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            return View(_unitOfWork.RecipeRepository.GetAll().Take(6).ToList());
         }
 
         public IActionResult Privacy()
---
+++ Presentation/Epicurious.MVC/Controllers/ProfileController.cs
@@ -0,0 +1,21 @@
+﻿using Epicurious.Domain.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Epicurious.MVC.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+
+        public ProfileController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> IndexAsync()
+        {
+            return View(await _userManager.GetUserAsync(User));
+        }
+    }
+}
